2c9ab2226365ff4c4cb150719892b933ad4ceb43
milktrader <milktrader@gmail.com>
2014-01-07 11:42:24 -0500

    added error message to all.jl that travis should pick up

--- src/Jig.jl
+++ src/Jig.jl
@@ -20,14 +20,17 @@ macro context(s)
 end
 
 function jtest(ex...)
+  fails = 0
   for e in ex
     if eval(:($e))
       print_with_color(:green, ".") 
     else
       print_with_color(:red, "x") 
+      fails += 1
     end
   end
   println("")
+  fails
 end
 
 end
